476069fc1ca9fffb41a684feea11af49ab28dc77
Kirikel <Mihaylenko04@gmail.com>
2023-11-21 13:07:09 +0500

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-import {createImagesData} from './data.js';
-import {setupImageGallery} from './gallery/main.js';
+import { createImagesData } from './data.js';
+import { setupImageGallery } from './gallery/main.js';
+import './upload/main.js';
 setupImageGallery(createImagesData());
